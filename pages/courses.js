4fb0433517760bb8c039b0803331c220819ed4ed
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-01-21 16:34:25 +0000

    Update courses.js

--- pages/courses.js
+++ pages/courses.js
@@ -1,27 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import Link from 'next/link'
 import fs from 'fs'
 import path from 'path'
-import styles from '../styles/Course.module.css'
 
-const fs = require('fs-web');
+export async function getServerSideProps(context) {
+  // read the .json file from the data directory
+  const filePath = path.join(process.cwd(), 'data', 'courses.json')
+  const data = JSON.parse(fs.readFileSync(filePath))
+  return { props: { courses: data } }
+}
 
-export default function Courses() {
-  export async function getServerSideProps(context) {
-  const [coursesList, setCourses] = useState([])
-
-  useEffect(() => {
-    // read the .json file from the data directory
-    const filePath = path.join(process.cwd(), 'data', 'courses.json')
-    const data = JSON.parse(fs.readFileSync(filePath))
-    setCourses(data)
-  }, [])
-
+export default function Courses({ courses }) {
   return (
     <div>
       <h1>Courses</h1>
       <ul>
-        {coursesList.map(course => (
+        {courses.map(course => (
           <li key={course.id}>
             <Link href={`/course?id=${course.id}`} as={`/courses/${course.id}`}>
               <a>{course.title}</a>
@@ -33,5 +25,3 @@ export default function Courses() {
     </div>
   )
 }
-}
-
