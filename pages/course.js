ee5a8d913537f1c9faf13fd5faa77e8068898074
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-01-21 16:35:09 +0000

    Update course.js

--- pages/course.js
+++ pages/course.js
@@ -1,30 +1,11 @@
 import fs from 'fs'
 import path from 'path'
 
-const fs = require('fs-web');
-
-export async function getStaticPaths() {
-    // read the .json file from the data directory
-    const filePath = path.join(process.cwd(), 'data', 'courses.json')
-    const data = JSON.parse(fs.readFileSync(filePath))
-    // paths will be the array of course ids
-    const paths = data.map(course => `/courses/${course.id}`)
+export async function getServerSideProps({ params }) {
+  // read the .json file from the data directory
+  const filePath = path.join(process.cwd(), 'data', `course-${params.id}.json`)
+  const data = JSON.parse(fs.readFileSync(filePath))
+  return { props: { course: data } }
 }
 
-export default function Course({ course }) {
-  return (
-    <div>
-      <h1>{course.title}</h1>
-      <p>{course.description}</p>
-      <div>
-        <h2>Lessons</h2>
-        {course.lessons.map(lesson => (
-          <div key={lesson.title}>
-            <h3>{lesson.title}</h3>
-            <span>Duration: {lesson.duration}</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
+export default function
